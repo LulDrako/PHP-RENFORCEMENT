ccce17832241b405262ec37288b0cf7432e8eeb5
Abderbess <146116459+Abderbess@users.noreply.github.com>
2024-09-09 16:03:19 +0200

    mvc/bdd

---
+++ model/bdd.php
@@ -0,0 +1,14 @@
+<?php
+
+class Bdd{
+
+    public static function connexion()
+    {
+        try {
+            $bdd= new PDO("mysql:host=localhost;port=3306;dbname=film","root","19211965");
+            return $bdd;
+        } catch (Exception $e) {
+            die( $e->getMessage());
+        }
+    }
+}
