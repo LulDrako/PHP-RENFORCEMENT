daec15d8aca5923cff83cfa32b6358a20a728e18
ervin <ervingoby@gmail.com>
2024-09-10 15:34:08 +0200

    bdd

--- model/bdd.php
+++ model/bdd.php
@@ -5,7 +5,7 @@ class Bdd{
     public static function connexion()
     {
         try {
-            $bdd= new PDO("mysql:host=localhost;port=3306;dbname=film","root","");
+            $bdd= new PDO("mysql:host=localhost;port=3306;dbname=film","root","768223");
             return $bdd;
         } catch (Exception $e) {
             die( $e->getMessage());
