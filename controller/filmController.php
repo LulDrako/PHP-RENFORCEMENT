248a7bb2638ac903d021c816e2bc74fc516c24fa
Abderbess <146116459+Abderbess@users.noreply.github.com>
2024-09-10 12:32:41 +0200

    version

--- controller/filmController.php
+++ controller/filmController.php
@@ -1,8 +1,13 @@
 <?php
 include_once("model/filmModel.php");
 
+class FilmController {
     public function getAccueilController()
     {
         $lastVelo = $this->model->dernieraccueilModel();
-        include("view/accueil.php");
-    }
+        include("view/home.php");
+    } 
+}
+
+
+    
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
 include("view/header.php");
 include("controller/router.php");
-include("view/footer.php");
-?>
+include("view/home.php");
