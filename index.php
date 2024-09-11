cabdcda9129bddaccd5f42e973e4cfa5fc6d4464
Rtmt92 <rayantoumert.rt@gmail.com>
2024-09-11 12:31:01 +0200

    error index fix

--- index.php
+++ index.php
@@ -5,9 +5,6 @@ include("views/header.php");
 // Inclusion du router (gère le contenu principal de la page)
 include("controller/router.php");
 
-// Si tu souhaites inclure un fichier spécifique pour le contenu de la page d'accueil
-include("view/home.php"); 
-
 // Inclusion du fichier footer (depuis la branche main)
 include("views/footer.php");
 ?>
