0e62f9bdde05ff478217c0fc15389061ac5be9bd
LulDrako <karimfeki2004@gmail.com>
2024-09-13 16:02:02 +0200

    Revert "bdd commit"
    This reverts commit fa09b3c0625bd6d7d649aa07f4f84f9033739c89.

--- model/bdd.php
+++ model/bdd.php
@@ -9,7 +9,7 @@ class Bdd {
     public static function connexion()
     {
         try {
-            $bdd= new PDO("mysql:host=localhost;port=3306;dbname=film","","");
+            $bdd= new PDO("mysql:host=localhost;port=3306;dbname=film","root","");
             return $bdd;
 
         } catch (Exception $e) {
