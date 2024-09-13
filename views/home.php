8b4c1cefe106f47f5aa8af1ea61c7cd4f7c9979c
ervin <ervingoby@gmail.com>
2024-09-13 14:48:05 +0200

    ho

--- views/home.php
+++ views/home.php
@@ -37,19 +37,7 @@ if (!empty($searchQuery)) {
                     <strong>Durée :</strong> <?= htmlspecialchars($film['movie_time']); ?><br>
                     <strong>Thème :</strong> <?= htmlspecialchars($film['theme']); ?><br>
                     <!-- Ajouter une image du film -->
-                    <img src="<?= htmlspecialchars($film['poster']); ?>" alt="Poster du film" style="width: 150px;"><br>
-
-                    <?php if (isset($_SESSION['user_id'])): ?>
-                        <!-- Ajouter un bouton pour aimer le film -->
-                        <form method="POST" action="index.php?page=like">
-    <input type="hidden" name="movie_id" value="<?= htmlspecialchars($film['id_movie']); ?>">
-    <button type="submit">Aimer</button>
-</form>
-
-
-                    <?php else: ?>
-                        <p><a href="index.php?page=login">Connectez-vous</a> pour aimer ce film.</p>
-                    <?php endif; ?>
+                    <img src="<?= htmlspecialchars($film['poster']); ?>" alt="Poster du film" style="width: 150px;">
                 </li>
             <?php endforeach; ?>
         </ul>
